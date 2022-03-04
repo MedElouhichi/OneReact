e6e1baf1c88ea699d7ef57a0e780f3b97e9385d0
Mohamed Elouhichi <95172051+MedElouhichi@users.noreply.github.com>
2022-03-04 15:37:12 +0000

    ibstall react.js

---
+++ resources/js/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+  render() {
+    return (
+    <div> {6+5} test ici </div>
+
+    )
+  }
+}
+
+
+export default App
+
+
+if(document.getElementById('root')){
+    ReactDOM.render(<App />,document.getElementById('root'));
+}
